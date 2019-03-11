ec45b8208089abd0463be7f8a3707321f7108d53
YongSeok <qazwsx1455@gmail.com>
2019-03-11 18:57:07 +0900

    2 주차 - 1

--- Widget/app/src/main/java/com/example/yongseok/widget/MainActivity.java
+++ Widget/app/src/main/java/com/example/yongseok/widget/MainActivity.java
@@ -1,13 +1,22 @@
-package com.example.yongseok.layout_1;
+package com.example.yongseok.widget;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView=findViewById(R.id.textView);
+        button = findViewById(R.id.button3);
+
+        textView.setText("Hello!!");
     }
 }
